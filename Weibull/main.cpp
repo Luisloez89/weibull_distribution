5db9b462e8848a6a048542ad66d7177fa2be2dd0
bhatmeister <bhatshubhs12@gmail.com>
2015-11-03 10:01:10 +0530

    Changed the author name.

--- Weibull/main.cpp
+++ Weibull/main.cpp
@@ -2,9 +2,7 @@
 //  main.cpp
 //  Weibull
 //
-//  Created by Sanjiv  Soni on 28/10/15.
-//  Copyright (c) 2015 sunjiv. All rights reserved.
-//
+
 
 #include <iostream>
 #include "weibull_estimator.h"
