e70a5ddf54783c18961032046852c1570b3552e5
bhatmeister <bhatshubhs12@gmail.com>
2015-11-03 19:43:43 +0530

    Added a macro for no. of inputs

--- Weibull/main.cpp
+++ Weibull/main.cpp
@@ -8,13 +8,16 @@
 #include "weibull_estimator.h"
 #include "weibull.h"
 #include "integration.h"
+#define N 6
 
 int main(int argc, const char * argv[])
 {
     float *data, *y;
     
-    data = new(float[6]);
-    y = new(float[6]);
+    data = new(float[N]);
+    y = new(float[N]);
+    
+    
     
     data[0] = 16;
     data[1] = 34;
@@ -23,19 +26,19 @@ int main(int argc, const char * argv[])
     data[4] = 93;
     data[5] = 120;
     
-    Estimator estimator(data, 6);
+    Estimator estimator(data, N);
     estimator.rankRegressionY();
     
-    Weibull weibull(data,6,estimator.getBeta(),estimator.getEta());
+    Weibull weibull(data,N,estimator.getBeta(),estimator.getEta());
     y = weibull.getDistribution();
     
-    std::cout<<weibull.getEta()<<"  "<<weibull.getBeta()<<"\n";
+    std::cout<<"Eta "<<weibull.getEta()<<" Beta  "<<weibull.getBeta()<<"\n";
     
-    for (int i = 0; i<6; i++) {
+    for (int i = 0; i<N; i++) {
         std::cout<<y[i]<<"\t";
     }
     
     std::cout<<"\nMean"<<weibull.mean()<<"\n";
 
-	system("pause"); //Not needed on Mac :p
+	//system("pause"); //Not needed on Mac :p
 }
